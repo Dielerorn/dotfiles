f0120770b7869f49a1784f7b644ab214b95ac4f2
Austin Larson <kingzap@larson-llc.com>
2018-04-03 17:09:21 -0600

    Add files via upload

---
+++ scripts/MonitorLayout.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#xrandr --output DP-3 --off --output DVI-I-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate left --output DP-5 --off --output DP-4 --off --output DVI-I-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-1 --off --output DP-0 --off
+
+xrandr --output DP-3 --off --output DVI-I-0 --off --output HDMI-1-2 --mode 1920x1080 --pos 840x-740 --rotate left --output DP-5 --off --output DP-4 --off --output DVI-I-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-1 --off --output DP-0 --off
